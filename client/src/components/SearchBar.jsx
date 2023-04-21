9f1b000b1f4b37f1e62d3104d24066bd75e657cf
Brian Shao <brianshao20@gmail.com>
2023-04-21 16:59:01 -0400

    Added Fetch for Geocoding api

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,13 +1,32 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
 
 export default function SearchBar() {
+
+    
+    function send(url) {
+        fetch(url)
+        .then((response) => console.log(response.json()))
+    }
+    const address = '1600 Pennsylvania Avenue NW, Washington, DC 20500';
+    const url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(address) + "&key=AIzaSyCAI3WTDKH75l6S7XDVRLMV-BvAGtxMdM4";
+    const handleEnter = (event) => {
+        //Line below prevents refresh on enter
+        event.preventDefault();
+        console.log("GOT here");
+        if(event.key === 'Enter') {
+            console.log("send message to google");
+            send(url);
+        }
+    }
+    function handleSearch() {
+        console.log("send message to GOOGLE");
+    }
+
   return (
     <Paper
       component="form"
@@ -20,8 +39,13 @@ export default function SearchBar() {
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search"
         inputProps={{ 'aria-label': 'search' }}
+        onKeyDown={handleEnter}
       />
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+      <IconButton type="button" 
+        sx={{ p: '10px' }} 
+        aria-label="search" 
+        onClick={handleSearch}
+        >
         <SearchIcon />
       </IconButton>
     </Paper>
