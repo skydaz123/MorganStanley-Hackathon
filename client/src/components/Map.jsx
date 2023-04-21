27db5dd2092f7f7ac1aad2b21f7b5e91167ea4b4
Brian Shao <brianshao20@gmail.com>
2023-04-21 15:06:14 -0400

    Map ui

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton, Menu, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Stack from '@mui/material/Stack';
+
 import {
   MapContainer,
   TileLayer,
@@ -11,6 +19,8 @@ import {
 // import 'leaflet-routing-machine/dist/leaflet-routing-machine.js';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import { Icon } from 'leaflet';
+import SearchBar from './SearchBar';
+
 
 const myIcon = new Icon({
   iconUrl: markerIcon,
@@ -18,9 +28,16 @@ const myIcon = new Icon({
   iconAnchor: [12, 41],
 });
 
+function handleClick() {
+  console.log("pressed")
+}
+
 export default function Map() {
   function MapC() {
     const map = useMap();
+    map.removeControl(map.zoomControl);
+    //comment below to remove Attribution text
+    map.attributionControl.setPrefix('');
     const routingControl = useRef(null);
 
     useMapEvent('click', (e) => {
@@ -33,18 +50,52 @@ export default function Map() {
   }
 
   return (
-    <div>
+    <div> 
+      <div style={{ display: 'flex', justifyContent: 'start'}}>
+        <Typography style={{ position: 'absolute', zIndex: '1000' }}> 
+          <Link to='/' 
+            style={{ color: 'inherit', textDecoration: 'none' }}
+          >
+           Name 
+          </Link>
+        </Typography>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center'}}> 
+        <div style={{ position: 'absolute', zIndex: '1000' }}>
+          <SearchBar />
+        </div>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'end'}}> 
+        <IconButton style={{ position: 'absolute', zIndex: '1000'}}
+        onClick={handleClick}> 
+          <MenuIcon sx = {{color: 'orange', fontSize: 50}}/>
+        </IconButton>
+      </div>
       <MapContainer
-        style={{ height: '95vh', width: '100wh' }}
+        style={{ height: '100vh', width: '100wh', position: 'relative'}}
         center={[0, 0]}
         zoom={3}
+
       >
-        <TileLayer
+        <TileLayer 
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         />
-        <MapC />
+        <MapC/>
       </MapContainer>
+      <div style={{ position: 'absolute', bottom: '20px', right: '20px', zIndex: '1000' }}>
+        <Stack spacing={2}>
+        <IconButton onClick={() => console.log('GPS clicked')} sx={{backgroundColor: 'white', borderRadius: 3}}>
+          <GpsFixedIcon />
+        </IconButton>
+        <IconButton onClick={() => console.log('Add clicked')} sx={{backgroundColor: 'white', borderRadius: 3}}>
+          <AddIcon />
+        </IconButton>
+        <IconButton onClick={() => console.log('Remove clicked')} sx={{backgroundColor: 'white', borderRadius: 3}}>
+          <RemoveIcon />
+        </IconButton>
+        </Stack>
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,6 +5,8 @@ import { useState } from "react"
 import { changeName } from "../redux/slices/demoSlice"
 import pika from "../images/surprised-pikachu.png"
 import RandomHeader from "../components/RandomHeader"
+import Map from "../components/Map"
+
 
 export default function Home() {
     const dispatch = useDispatch()
@@ -22,29 +24,7 @@ export default function Home() {
 
     return (
         <Stack spacing="16px" margin="16px">
-            <Typography variant="h6">Hello, this is the home page.</Typography>
-            <Typography>It was lazily loaded.</Typography>
-            <Typography>This is some data on the redux store: "name={name}"</Typography>
-            <Typography>You can control that data down here:</Typography>
-            <Stack spacing="8px" direction="row">
-                <TextField
-                    label="Name"
-                    value={inputName}
-                    onChange={handleChange}
-                />
-                <Button onClick={sendName}>Submit</Button>
-            </Stack>
-            <Stack spacing="8px" direction="row">
-                <Typography>Here is an imported image:</Typography>
-                <Box>
-                    <img
-                        src={pika}
-                        alt="Surprised Pikachu"
-                        width="200px"
-                    />
-                </Box>
-            </Stack>
-            <RandomHeader name="some component"/>
+            <Map/>
         </Stack>
     )
 }
