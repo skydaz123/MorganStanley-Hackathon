f361d8851a96fd7f3fa66ed1896e99978ae9c22e
Hou <64312205+SciencePotato@users.noreply.github.com>
2023-04-22 07:44:58 -0400

    Proxy added, config through .env

--- client/src/firebase_config.js
+++ client/src/firebase_config.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import axios from "axios";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAxdUYt0iCeAYT_iA2jldonPYZWsYdhT1w",
-  authDomain: "codetogive5.firebaseapp.com",
-  projectId: "codetogive5",
-  storageBucket: "codetogive5.appspot.com",
-  messagingSenderId: "559532548129",
-  appId: "1:559532548129:web:0e39b1c16be463fce74360",
-  measurementId: "G-P9H3JVBBTR"
-};
+let app;
+let analytics;
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+axios.get('/firebase').then((res) => {
+  const firebaseConfig = res.data
+  app = initializeApp(firebaseConfig) 
+  analytics = getAnalytics(app);
+})
