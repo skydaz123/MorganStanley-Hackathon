393091f3867f7b85eda52492c0db60795bacf5e3
Benny Liu <liubenny09@gmail.com>
2023-04-22 11:03:43 -0400

    first commit

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,8 +2,15 @@ import AddIcon from '@mui/icons-material/Add';
 import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 import MenuIcon from '@mui/icons-material/Menu';
 import RemoveIcon from '@mui/icons-material/Remove';
-import { IconButton, Typography, Modal } from '@mui/material'; // Import Modal here
-import Stack from '@mui/material/Stack';
+import {
+  IconButton,
+  Typography,
+  Modal,
+  Stack,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material'; // Import Modal here
 import L, { Icon } from 'leaflet';
 // import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 // import 'leaflet-routing-machine/dist/leaflet-routing-machine.js';
@@ -20,6 +27,16 @@ const myIcon = new Icon({
     iconSize: [25, 41],
     iconAnchor: [12, 41],
 });
+
+const mockDailyData = {
+  storageRemaining: 75, // %
+  distances: [
+    { partner: 'Location A', distance: 5 }, // km
+    { partner: 'Location B', distance: 7 }, // km
+    { partner: 'Location C', distance: 3 }, // km
+  ],
+  foodShipmentRequests: [1, 0, 1, 1, 0], // last 5 days (1 if request that day, 0 otherwise)
+};
 
 const southWest = L.latLng(-90, -180);
 const northEast = L.latLng(90, 180);
@@ -51,38 +68,44 @@ export default function Map() {
           name: '466 Northside Dr NW, Atlanta, GA 30318',
           lat: 33.7671923,
           lng: -84.40537119999999,
+          dailyData: mockDailyData,
         },
         {
           name: '1122A Old Chattahoochee Ave NW # A, Atlanta, GA 30318',
           lat: 33.79994,
           lng: -84.42485099999999,
+          dailyData: mockDailyData,
         },
         {
           name: '246 Sycamore St, Decatur, GA 30030',
           lat: 33.7749219,
           lng: -84.2929674,
+          dailyData: mockDailyData,
         },
         {
           name: '2514 W Point Ave, Atlanta, GA 30337',
           lat: 33.627911,
           lng: -84.4715296,
+          dailyData: mockDailyData,
         },
         {
           name: '921 Howell Mill Rd NW, Atlanta, GA 30318',
           lat: 33.78034239999999,
           lng: -84.410242,
+          dailyData: mockDailyData,
         },
         {
           name: '1560 Memorial Dr SE, Atlanta, GA 30317',
           lat: 33.7485041,
           lng: -84.3365784,
+          dailyData: mockDailyData,
         },
       ];
     
       useEffect(() => {
         locations.forEach((loc) => {
           const marker = L.marker([loc.lat, loc.lng], { icon: myIcon }).addTo(map);
-          marker.bindPopup(loc.name).on('click', () => handleOpenModal(loc.name));
+          marker.bindPopup(loc.name).on('click', () => handleOpenModal(loc));
         });
       }, [locations, map]);
     
@@ -91,13 +114,41 @@ export default function Map() {
 
     return (
         <div>
-          <Modal open={isModalOpen} onClose={handleCloseModal}>
+        <Modal open={isModalOpen} onClose={handleCloseModal}>
+        {selectedLocation ? (
           <div style={{ padding: '20px', backgroundColor: 'white' }}>
-            <h2>Location</h2>
-            <p>{selectedLocation}</p>
+            <List>
+            <h2>{selectedLocation?.name}</h2>
+              <ListItem>
+                <ListItemText primary={`Storage Remaining: ${selectedLocation?.dailyData.storageRemaining}%`} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Distances to Other Partners:" />
+                <List>
+                  {selectedLocation?.dailyData.distances.map((distance, index) => (
+                    <ListItem key={index}>
+                      <ListItemText primary={`${distance.partner}: ${distance.distance} km`} />
+                    </ListItem>
+                  ))}
+                </List>
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Food Shipment Requests (Last 5 Days):" />
+                <List>
+                  {selectedLocation?.dailyData.foodShipmentRequests.map((request, index) => (
+                    <ListItem key={index}>
+                      <ListItemText primary={request ? 'Requested' : 'Not Requested'} />
+                    </ListItem>
+                  ))}
+                </List>
+              </ListItem>
+            </List>
             <button onClick={handleCloseModal}>Close</button>
           </div>
-        </Modal>
+        ) : (
+          <div></div> // Fallback empty element
+        )}
+      </Modal>
             <div style={{ display: 'flex', justifyContent: 'start' }}>
                 <Typography style={{ position: 'absolute', zIndex: '1000' }}>
                     <Link to='/'
