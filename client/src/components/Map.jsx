9fb33afce7d3d85aae0d03574cc11469ac0f4d94
Benny Liu <liubenny09@gmail.com>
2023-04-22 19:43:45 -0400

    changes to line graph

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -24,6 +24,15 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import MapDrawer from './MapDrawer';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 
 
 const myIcon = new Icon({
@@ -32,14 +41,23 @@ const myIcon = new Icon({
     iconAnchor: [12, 41],
 });
 
+function calculateStorageQuantity(lastDeliveryDate) {
+  const currentDate = new Date();
+  const lastDelivery = new Date(lastDeliveryDate);
+  const daysSinceLastDelivery = Math.floor((currentDate - lastDelivery) / (1000 * 60 * 60 * 24));
+
+  const initialStorageQuantity = 150;
+  const dailyConsumptionRate = 10;
+
+  const currentStorageQuantity = initialStorageQuantity - (daysSinceLastDelivery * dailyConsumptionRate);
+
+  // If the storage quantity falls below 0, set it to 0
+  return currentStorageQuantity > 0 ? currentStorageQuantity : 0;
+}
+
 const mockDailyData = {
-  storageRemaining: 75, // %
-  distances: [
-    { partner: 'Location A', distance: 5 }, // km
-    { partner: 'Location B', distance: 7 }, // km
-    { partner: 'Location C', distance: 3 }, // km
-  ],
-  foodShipmentRequests: [1, 0, 1, 1, 0], // last 5 days (1 if request that day, 0 otherwise)
+  storageQuantity: 150, // Quantity
+  lastRequestedDates: ['2023-04-18', '2023-04-15', '2023-04-14'], // Dates for when food was last requested
 };
 
 const southWest = L.latLng(-90, -180);
@@ -48,6 +66,15 @@ const bounds = L.latLngBounds(southWest, northEast);
 
 function handleClick() {
     console.log("pressed")
+}
+
+function generateGraphData(location) {
+  const data = location.dailyData.lastRequestedDates.map((date) => {
+    const storageQuantity = calculateStorageQuantity(date);
+    return { date, storageQuantity };
+  });
+
+  return data;
 }
 
 let maps = null;
@@ -76,37 +103,61 @@ export default function Map() {
           name: '466 Northside Dr NW, Atlanta, GA 30318',
           lat: 33.7671923,
           lng: -84.40537119999999,
-          dailyData: mockDailyData,
+          lastDeliveryDate: '2023-04-15',
+          dailyData: {
+            storageQuantity: calculateStorageQuantity('2023-04-15'),
+            lastRequestedDates: ['2023-04-18', '2023-04-15', '2023-04-14'],
+          },
         },
         {
           name: '1122A Old Chattahoochee Ave NW # A, Atlanta, GA 30318',
           lat: 33.79994,
           lng: -84.42485099999999,
-          dailyData: mockDailyData,
+          lastDeliveryDate: '2023-04-12',
+          dailyData: {
+            storageQuantity: calculateStorageQuantity('2023-04-12'),
+            lastRequestedDates: ['2023-04-18', '2023-04-15', '2023-04-14'],
+          },
         },
         {
           name: '246 Sycamore St, Decatur, GA 30030',
           lat: 33.7749219,
           lng: -84.2929674,
-          dailyData: mockDailyData,
+          lastDeliveryDate: '2023-04-10',
+          dailyData: {
+            storageQuantity: calculateStorageQuantity('2023-04-10'),
+            lastRequestedDates: ['2023-04-18', '2023-04-15', '2023-04-14'],
+          },
         },
         {
           name: '2514 W Point Ave, Atlanta, GA 30337',
           lat: 33.627911,
           lng: -84.4715296,
-          dailyData: mockDailyData,
+          lastDeliveryDate: '2023-04-18',
+          dailyData: {
+            storageQuantity: calculateStorageQuantity('2023-04-18'),
+            lastRequestedDates: ['2023-04-18', '2023-04-15', '2023-04-14'],
+          },
         },
         {
           name: '921 Howell Mill Rd NW, Atlanta, GA 30318',
           lat: 33.78034239999999,
           lng: -84.410242,
-          dailyData: mockDailyData,
+          lastDeliveryDate: '2023-04-17',
+          dailyData: {
+            storageQuantity: calculateStorageQuantity('2023-04-17'),
+            lastRequestedDates: ['2023-04-18', '2023-04-15', '2023-04-14'],
+          },
         },
         {
           name: '1560 Memorial Dr SE, Atlanta, GA 30317',
           lat: 33.7485041,
           lng: -84.3365784,
-          dailyData: mockDailyData,
+          lastDeliveryDate: '2023-04-20',
+          dailyData: {
+            storageQuantity: calculateStorageQuantity('2023-04-20'),
+            lastRequestedDates: ['2023-04-18', '2023-04-15', '2023-04-14'],
+          },
         },
       ];
 
@@ -218,49 +269,68 @@ export default function Map() {
     }
     return (
         <div>
-          <Modal open={isModalOpen} onClose={handleCloseModal}>
-        {selectedLocation ? (
-          <div style={{ padding: '20px', backgroundColor: 'white' }}>
-            <List>
-              <h2>{selectedLocation.name}</h2>
-              <ListItem>
-                <ListItemText primary={`Storage Remaining: ${selectedLocation.dailyData.storageRemaining}%`} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="Distances to Other Partners:" />
-                <List>
-                  {selectedLocation.dailyData.distances.map((distance, index) => (
-                    <ListItem key={index}>
-                      <ListItemText primary={`${distance.partner}: ${distance.distance} km`} />
+            <Modal open={isModalOpen} onClose={handleCloseModal}>
+              {selectedLocation ? (
+                <div style={{ padding: '20px', backgroundColor: 'white', color: 'orange' }}>
+                  <List>
+                    <h2>{selectedLocation.name}</h2>
+                    <ListItem>
+                      <ListItemText primary={`Storage Quantity: ${selectedLocation.dailyData.storageQuantity}`} />
                     </ListItem>
-                  ))}
-                </List>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="Food Shipment Requests (Last 5 Days):" />
-                <List>
-                  {selectedLocation.dailyData.foodShipmentRequests.map((request, index) => (
-                    <ListItem key={index}>
-                      <ListItemText primary={request ? 'Requested' : 'Not Requested'} />
+                    <ListItem>
+                      <ListItemText primary="Food Shipment Requests (Last Requested Dates):" />
+                      <List>
+                        {selectedLocation.dailyData.lastRequestedDates.map((date, index) => (
+                          <ListItem key={index}>
+                            <ListItemText primary={date} />
+                          </ListItem>
+                        ))}
+                      </List>
                     </ListItem>
-                  ))}
-                </List>
-              </ListItem>
-            </List>
-            <button onClick={handleCloseModal}>Close</button>
-          </div>
-        ) : (
-          <div></div> // Fallback empty element
-        )}
-      </Modal>
+                  </List>
+                  <LineChart
+                    width={600}
+                    height={300}
+                    data={generateGraphData(selectedLocation)}
+                    margin={{
+                      top: 5,
+                      right: 30,
+                      left: 20,
+                      bottom: 5,
+                    }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                      type="monotone"
+                      dataKey="storageQuantity"
+                      stroke="#8884d8"
+                      activeDot={{ r: 8 }}
+                    />
+                  </LineChart>
+                  <button onClick={handleCloseModal} style={{ backgroundColor: 'orange', color: 'white' }}>Close</button>
+                </div>
+              ) : (
+                <div></div> // Fallback empty element
+              )}
+            </Modal>
+
             <div style={{ display: 'flex', justifyContent: 'start' }}>
-                <Typography style={{ position: 'absolute', zIndex: '1000', fontSize: '40px', color: 'Red', marginTop: '1.25%'  }}>
-                    <Link to='/'
-                          style={{ color: 'inherit', textDecoration: 'none' }}
-                    >
-                        Name
-                    </Link>
-                </Typography>
+              <Typography style={{
+                position: 'absolute',
+                zIndex: '1000',
+                fontSize: '40px',
+                color: 'orange',
+                marginTop: '1.25%',
+                textShadow: '1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black'  // Add text-shadow here
+            }}>
+              <Link to='/' style={{ color: 'inherit', textDecoration: 'none' }}>
+                Name
+              </Link>
+            </Typography>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center'}}>
                 <div style={{ position: 'absolute', zIndex: '1000', marginTop: '2.5%' }}>
