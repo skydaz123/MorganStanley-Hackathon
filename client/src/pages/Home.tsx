7af51bb056c7e14a015e10b99ac5294157ffdc84
Hao Duong <haowdy26@gmail.com>
2023-04-22 02:07:42 -0400

    Finished SignUp component for registering to be a partner

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,13 +2,16 @@ import LoginComponent from "../components/LoginComponent";
 import RegisterComponent from "../components/RegisterComponent"
 import Box from '@mui/material/Box';
 import ReportComponent from "../components/ReportComponent";
+import SignUp from "../components/SignUp";
 
 export default function Home() {
     return (
         <Box sx={{ display: 'flex' }}>
-            <LoginComponent/>
-            <RegisterComponent/>
-            <ReportComponent/>
+
+            {/*<LoginComponent/>*/}
+            {/*<RegisterComponent/>*/}
+            {/*<ReportComponent/>*/}
+            <SignUp />
         </Box>
     )
 }
