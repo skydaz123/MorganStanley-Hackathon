92a31c9209851b037267808f683437bc6792ddb7
Phi Long Bui <longphibui1993@gmail.com>
2023-04-23 13:41:39 -0400

    update partner admin

--- client/src/pages/partner/Stats.tsx
+++ client/src/pages/partner/Stats.tsx
@@ -11,16 +11,56 @@ import {
 } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from "@devexpress/dx-react-chart";
 import Grid from "@mui/material/Grid";
+import { useDispatch, useSelector } from "react-redux"
+import { useLazyGetReportsQuery } from "../../redux/apis/localApi/firebaseApi"
+import { getAuthSlice } from "../../redux/store"
+import { useState, useEffect } from 'react'
 
 export default function Stats() {
+    const state = useSelector(getAuthSlice)
+    const [loading, setLoading] = useState(false)
+    const [getReports] = useLazyGetReportsQuery();
+    const [recievedData, setRecievedData] = useState([]);
+    const [givenData, setGivenData] = useState([]);
+
+    const getData = async(token: string) => {
+        console.log("BeepBooop");
+        const result = await getReports(token).unwrap();
+
+        let foodRecievedData = result.map((x: {
+            timestamp: any; lb_recieved: any; lb_given: any; 
+            }, index: any) => {
+            return {
+                value: x.lb_recieved,
+                argument: x.timestamp
+            }
+        });
+        foodRecievedData.sort((a: { argument: any; }, b: { argument: any; }) => a.argument - b.argument);
+        console.log(foodRecievedData)
+
+        let foodGivenData = result.map((x: {
+            timestamp: any; lb_recieved: any; lb_given: any; 
+            }, index: any) => {
+            return {
+                value: x.lb_given,
+                argument: x.timestamp
+            }
+        });
+        foodGivenData.sort((a: { argument: any; }, b: { argument: any; }) => a.argument - b.argument);
+        console.log(foodGivenData);
+
+        setRecievedData(foodRecievedData);
+        setGivenData(foodGivenData);
+    }
+
+    useEffect(() => {
+        // @ts-ignore
+        let token: string = state.user.token;
+        getData(token);
+    }, [])
+
     useHideSlidingWindowOnLoad()
-    const data = [
-        {argument: 'Monday', value: 30},
-        {argument: 'Tuesday', value: 20},
-        {argument: 'Wednesday', value: 10},
-        {argument: 'Thursday', value: 50},
-        {argument: 'Friday', value: 60},
-    ];
+
 
     return (
         <Box>
@@ -38,12 +78,12 @@ export default function Stats() {
                 <Grid item xs={6}>
                     <Box sx={{ border: '2px solid #EC701B', borderRadius: 2 }}>
                         <Chart
-                            data={data}
+                            data={recievedData}
                             height={400}
                         >
                             <ArgumentAxis/>
                             <ValueAxis/>
-                            <BarSeries valueField="value" argumentField="argument" color="#FF9600"/>
+                            <SplineSeries valueField="value" argumentField="argument" color="#FF9600"/>
                             <Title text="Food Received (in lbs)" />
                             <Animation />
                         </Chart>
@@ -52,7 +92,7 @@ export default function Stats() {
                 <Grid item xs={5.5}>
                     <Box sx={{ border: '2px solid #EC701B', borderRadius: 2 }}>
                         <Chart
-                            data={data}
+                            data={givenData}
                             height={400}
                         >
                             <ArgumentAxis/>
